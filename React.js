a6609710c4185ae0e0edbc590c5e1684ea8687aa
Barakat Elsaeed <159019040+BarakatElsaeed@users.noreply.github.com>
2024-03-02 11:54:20 +0200

    Add files via upload

---
+++ React.js
@@ -0,0 +1,21 @@
+const DUMMY_DATdjhfbmasA = [{}
+]
+<React />
+<React />
+<React />
+<React />
+<React />
+<React />
+<React />
+<React />
+const React = () => {
+    
+{if(DUMMY_DATA.length === 0) {<div></div>}else{DUMMY_DATA.map(item => <div>
+    <h1>{item.name}</h1>
+    <h4>{item.price}</h4>
+</div>)
+}}
+
+};
+export default React;
+
